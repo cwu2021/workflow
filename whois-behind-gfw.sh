56376ece25cc6043bdd8ad9c1a8cffd718a1928a
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2021-12-09 13:23:28 +0800

    observation of PTT via gfw

---
+++ whois-behind-gfw.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Cisco’s Latest Attempt to Dodge Responsibility for Facilitating Human Rights Abuses: Export Rules
+# https://www.eff.org/deeplinks/2016/04/ciscos-latest-attempt-dodge-responsibility-facilitating-human-rights-abuses-export
+
+# Also some case observation from PTT, a Taiwanese BBS which reveals ALL users' geoip.
+# https://www.ptt.cc/bbs/WorkinChina/M.1638802828.A.995.html
+
+whois 101.87.102.26
+
+: '
+descr:          CHINANET SHANGHAI PROVINCE NETWORK
+descr:          China Telecom
+descr:          No.31,jingrong street
+descr:          Beijing 100032
+country:        CN
+...
+person:         Weng Wen Qian
+address:        Room 2405,357 Songlin Road,Shanghai 200122
+country:        CN
+phone:          +86-21-68405784
+fax-no:         +86-21-50623458
+e-mail:         wengwq@online.sh.cn
+nic-hdl:        WWQ4-AP
+mnt-by:         MAINT-CHINANET-SH
+last-modified:  2008-09-04T07:34:05Z
+source:         APNIC
+'
