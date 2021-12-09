bf5f8122b347356b0a90d9c7b22dcfa5378c8e4b
cwu2021 <90231918+cwu2021@users.noreply.github.com>
2021-12-09 15:57:24 +0800

    diff get suspicious blocks.

--- flow-ipblocks.sh
+++ flow-ipblocks.sh
@@ -7,3 +7,7 @@ workdir=/data/flows/$year/$year-$month/$year-$month-$day
 cd $workdir
 # (Dec 5, 2021) destination ip sort to /16, as rough comparison to zone file.
 flow-cat ft* | flow-print -n | awk '{print $2}'| grep -v unwanted.ipblock | cut -d '.' -f1,2 | sort -u
+# (Dec 9, 2021) diff '<' twice to get suspicious blocks. however, /16 may be too wide.
+flow-cat ft* | flow-print -n | grep '168\.95\.'
+# {'country_code': 'TW', 'country_name': 'Taiwan', 'city': None, 'postal': None,
+# 'latitude': 23.5, 'longitude': 121, 'IPv4': '168.95.1.1', 'state': None}
